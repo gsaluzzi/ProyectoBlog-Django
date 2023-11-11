563a8f9b894cffba1fc7049c05e1d268a01165d3
Giorgio Saluzzi <giosaluzzi@gmail.com>
2023-11-11 12:45:17 -0300

    modelo crud funcionando

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,14 @@
 from home.views import home, about
 from django.urls import path
-from home.views import ListadoCriticas, CrearCritica
+from home.views import ListadoCriticas, CrearCritica, ActualizarCritica, DetalleCritica, EliminarCritica
 
 urlpatterns = [
     path('', home, name="home"),
     path('about/', about, name="about" ),
     path('criticas/', ListadoCriticas.as_view(), name='criticas'),
     path('criticas/crear/', CrearCritica.as_view(), name='crear_critica'),
+    path('criticas/<int:pk>/', DetalleCritica.as_view(), name='detalle_critica'),
+    path('criticas/<int:pk>/actualizar/', ActualizarCritica.as_view(), name='actualizar_critica'),
+    path('criticas/<int:pk>/eliminar/', EliminarCritica.as_view(), name='eliminar_critica'),
    
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from home.models import Criticas
+
 
 def home(request):
     
@@ -27,6 +29,24 @@ class CrearCritica(CreateView):
     model = Criticas
     template_name = "home/crear_criticas.html"
     fields = ['titulo', 'critica', 'autor', 'fecha_creacion']
+    success_url = reverse_lazy('criticas')
+
+
+class ActualizarCritica(UpdateView):
+    model = Criticas
+    template_name = "home/actualizar_criticas.html"
+    fields = ['titulo', 'critica', 'autor', 'fecha_creacion']
+    success_url = reverse_lazy('criticas')
+
+
+class DetalleCritica(DetailView):
+    model = Criticas
+    template_name = "home/detalle_critica.html"
+
+
+class EliminarCritica(DeleteView):
+    model = Criticas
+    template_name = "home/eliminar_critica.html"
     success_url = reverse_lazy('criticas')
 
 
